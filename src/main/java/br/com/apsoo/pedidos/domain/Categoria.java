f100ba01b7e212070509c6ec9b577c11da6e8d3b
Cleiton Gonçalves de Almeida <cgalmeida@azi.com.br>
2019-04-09 22:28:25 -0400

    Criação do projeto pedidos com conexão ao banco de dados PostgreSQL, classe Categoria e uma camada de controladores Rest como exemplo.

---
+++ src/main/java/br/com/apsoo/pedidos/domain/Categoria.java
@@ -0,0 +1,65 @@
+package br.com.apsoo.pedidos.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_CATEGORIA")
+@SequenceGenerator(name = "seq_categoria")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "CA_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_categoria")
+    private Integer id;
+
+    @Column(name = "CA_NOME")
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
