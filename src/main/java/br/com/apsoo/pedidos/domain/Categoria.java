458a9e8af4d6bd43a92d5c9c6acf4dc3bb512bf9
Cleiton Gonçalves de Almeida <cgalmeida@azi.com.br>
2019-04-16 22:39:00 -0400

    - Criação do endpoint de categorias e seus produtos por id - Criação da entidade Produto - Tratamento de erro 404

--- src/main/java/br/com/apsoo/pedidos/domain/Categoria.java
+++ src/main/java/br/com/apsoo/pedidos/domain/Categoria.java
@@ -5,9 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -24,6 +27,9 @@ public class Categoria implements Serializable {
 
     @Column(name = "CA_NOME")
     private String nome;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
 
@@ -48,6 +54,14 @@ public class Categoria implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
---
+++ src/main/java/br/com/apsoo/pedidos/domain/Produto.java
@@ -0,0 +1,101 @@
+package br.com.apsoo.pedidos.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+@SequenceGenerator(name = "seq_produto")
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "PR_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_produto")
+    private Integer id;
+
+    @Column(name = "PR_NOME")
+    private String nome;
+
+    @Column(name = "PR_PRECO")
+    private Double preco;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "TB_PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "PR_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CA_ID")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto(){
+
+    }
+
+    public Produto(Integer id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
